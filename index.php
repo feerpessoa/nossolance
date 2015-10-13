d6b22eec1c8b57a2879cdbe2ebd4597706040827
feerpessoa.j@hotmail.com <fernandapessoa@MacBook-Air-de-Fernanda.local>
2015-10-13 19:27:09 -0300

    nosso lance

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Nosso Lance</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<section id="header">
+				<header>
+					<h1>NOSSO LANCE</h1>
+					<p>Você pede, a gente dá o lance!</p>
+				</header>
+				<footer>
+					<a href="#banner" class="button style2 scrolly-middle">Saiba mais sobre a gente</a>
+				</footer>
+			</section>
+
+		<!-- Banner -->
+			<section id="banner">
+				<header>
+					<h2>Aqui nós encontramos tudo que você precisa</h2>
+				</header>
+				<p>Está precisando de algum tipo de serviço para sua empresa, casa ou escritório?<br />
+				É muito fácil, você dá o <strong>lance</strong>, e nós procuramos os melhores profissionais...<br />
+				Aí é só escolher qual <strong>lance</strong> é o melhor para você.</p>
+				<footer>
+					<a href="#first" class="button style2 scrolly">Conheça alguns de nossos serviços</a>
+				</footer>
+			</section>
+
+		<!-- Feature 1 -->
+			<article id="first" class="container box style1 right">
+				<a href="#" class="image fit"><img src="images/pic01.jpg" alt="" /></a>
+				<div class="inner">
+					<header>
+						<h2>Vai sair e precisa de alguém pra cuidar do filho? Chame a babá</h2>
+					</header>
+					<p>Aqui no <strong>Nosso Lance</strong> você encontra as melhores babás para que você saia 
+					e fique tranquilo em deixar o seu bebê seguro.</p>
+				</div>
+			</article>
+
+		<!-- Feature 2 -->
+			<article class="container box style1 left">
+				<a href="#" class="image fit"><img src="images/pic02.jpg" alt="" /></a>
+				<div class="inner">
+					<header>
+						<h2>O computador estragou? Nós oferecemos Serviços de Informática</h2>
+					</header>
+					<p>A gente encontra os melhores técnicos em informática pra consertar o seu computador e
+						você continuar seu trabalho tranquilamente.</p>
+				</div>
+			</article>
+
+		<!-- Portfolio -->
+			<article class="container box style2">
+				<header>
+					<h2>Outros serviços</h2>
+					<p>Esses são apenas alguns <br />
+					dos serviços oferecidos pelo Nosso Lance</p>
+				</header>
+				<div class="inner gallery">
+					<div class="row 0%">
+						<div class="3u 12u(mobile)"><a href="images/fulls/01.jpg" class="image fit"><img src="images/thumbs/01.jpg" alt="" title="Encanador" /></a></div>
+						<div class="3u 12u(mobile)"><a href="images/fulls/02.jpg" class="image fit"><img src="images/thumbs/02.jpg" alt="" title="Digitador e formatador de trabalhos" /></a></div>
+						<div class="3u 12u(mobile)"><a href="images/fulls/03.jpg" class="image fit"><img src="images/thumbs/03.jpg" alt="" title="Decorador de festas" /></a></div>
+						<div class="3u 12u(mobile)"><a href="images/fulls/04.jpg" class="image fit"><img src="images/thumbs/04.jpg" alt="" title="Artesão" /></a></div>
+					</div>
+					<div class="row 0%">
+						<div class="3u 12u(mobile)"><a href="images/fulls/05.jpg" class="image fit"><img src="images/thumbs/05.jpg" alt="" title="Cabelereiro" /></a></div>
+						<div class="3u 12u(mobile)"><a href="images/fulls/06.jpg" class="image fit"><img src="images/thumbs/06.jpg" alt="" title="Marido de Aluguel" /></a></div>
+						<div class="3u 12u(mobile)"><a href="images/fulls/07.jpg" class="image fit"><img src="images/thumbs/07.jpg" alt="" title="Manicure" /></a></div>
+						<div class="3u 12u(mobile)"><a href="images/fulls/08.jpg" class="image fit"><img src="images/thumbs/08.jpg" alt="" title="Barbeiro" /></a></div>
+					</div>
+				</div>
+			</article>
+
+		<!-- Contact -->
+			<article class="container box style3">
+				<header>
+					<h2>Dúvidas?</h2>
+					<p>Entre em contato com a gente</p>
+				</header>
+				<span> sac@nossolance.com.br</span>
+			</article>
+
+		<section id="footer">
+			<ul class="icons">
+				<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+				<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+				<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+				<li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
+				<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+				<li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+			</ul>
+			<div class="copyright">
+				<ul class="menu">
+					<li>2015 &copy; NossoLance Soluções e Serviços. Todos os direitos reservados.</li><li>Design: 8º Período Noturno - Sistemas de Informação UNIPAM</li>
+				</ul>
+			</div>
+		</section>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
